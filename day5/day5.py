e968707158c4ef4b3799e29c9618f204c1be96d9
Apple <102968904+Applee-Chen@users.noreply.github.com>
2022-12-05 23:31:29 +0000

    List of 9 stacks

---
+++ day5/day5.py
@@ -0,0 +1,21 @@
+# Write the state of 9 stacks into 9 lists 
+Stack = []
+Stack_1 = ['S','Z','P','D','L','B','F','C']
+Stack_2 = ['N','V','G','P','H','W','B']
+Stack_3 = ['F','W','B','J','G']
+Stack_4 = ['G','J','N','F','L','W','C','S']
+Stack_5 = ['W','J','L','T','P','M','S','H']
+Stack_6 = ['B','C','W','G','F','S']
+Stack_7 = ['H','T','P','M','Q','B','W']
+Stack_8 = ['F','S','W','T']
+Stack_9 = ['N','C','R']
+Stack.append(Stack_1)
+Stack.append(Stack_2)
+Stack.append(Stack_3)
+Stack.append(Stack_4)
+Stack.append(Stack_5)
+Stack.append(Stack_6)
+Stack.append(Stack_7)
+Stack.append(Stack_8)
+Stack.append(Stack_9)
+
