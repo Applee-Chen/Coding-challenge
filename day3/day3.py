475b2243e29a0ce83e25d119d4e8ceefb98c284d
Apple <102968904+Applee-Chen@users.noreply.github.com>
2022-12-05 20:21:49 +0000

    Day 3 extension

--- day3/day3.py
+++ day3/day3.py
@@ -6,6 +6,16 @@ def check_items(s):
             if s[i] == s[compartment_length+j]:
                 return(s[i])
 
+# Write a function that finds the character in common among 3 elves
+def check_elf_items(a,b,c):
+    for j in range(int(len(a) - 1)):
+        for k in range(int(len(b) - 1)):
+            if a[j] == b[k]:
+                for z in range(int(len(c) - 1)):
+                    if a[j] == c[z]:
+                        return(a[j])
+                          
+
 # Write a function that calculates priorities for each rucksack
 def priority_calculator(r):
     if ord(r) < 97:
@@ -19,8 +29,15 @@ items = []
 with open('day3.txt', 'r', encoding='utf-8') as file:
     items = file.readlines()
 
+# Task 1
 PRIORITY = 0
 for r in items:
     PRIORITY = PRIORITY + priority_calculator(check_items(r))
 print(PRIORITY)
 
+#Task 2
+Total_priority = 0
+for i in range(100):
+    Total_priority= Total_priority + priority_calculator(check_elf_items(items[3*i],items[3*i + 1],items[3*i + 2]))
+
+print(Total_priority)
