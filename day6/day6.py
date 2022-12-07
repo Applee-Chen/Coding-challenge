eb5abcebc9f770187ac829e940c7533f3d5b0b9f
Apple <102968904+Applee-Chen@users.noreply.github.com>
2022-12-07 15:03:24 +0000

    day 6 part 1

--- day6/day6.py
+++ day6/day6.py
@@ -1,4 +1,17 @@
 # Write a function that checks if the consecutive 4 characters are distinct
 def check(s):
-    for i in range(int(len(s)) - 3):
-        for j in range()
+    counter = 0
+    for i in range(int(len(s) - 3)):
+        counter = counter + 1
+        if s[i] != s[i + 1] and s[i] != s[i + 2] and s[i] != s[i + 3]:
+            if s[i + 1] != s[i + 2] and s[i + 1] != s[i + 3]:
+                if s[i + 2] != s[i + 3]:
+                    return(int(counter + 3))
+
+            
+# Day 6 task 1
+Data = []
+with open('day6.txt', 'r', encoding='utf-8') as file:
+    Data = file.read()
+
+print(check(Data))
