52ba1fb9ebbbb37d96070474c339a340711b16a4
Apple <102968904+Applee-Chen@users.noreply.github.com>
2022-12-14 22:26:10 +0000

    part 2

--- day10/day10.py
+++ day10/day10.py
@@ -26,7 +26,30 @@ Actions = []
 with open('day10.txt', 'r', encoding='utf-8') as file:
     Actions = file.readlines()
 List = converter(Actions)
-
+print(List)
 # Task 1
 answer1 = signal_strengths_calculator(20,List) + signal_strengths_calculator(60,List) + signal_strengths_calculator(100,List) + signal_strengths_calculator(140,List) + signal_strengths_calculator(180,List) + signal_strengths_calculator(220,List)
-print(answer1)
+
+# Task 2
+Image = []
+# Write a function that only return the register X
+def signal_calculator(i,file):
+    X = 1
+    if i>=2:
+        for j in range(i-1):
+            X = X + file[j]
+    return(X)
+
+for k in range(240):
+    if k == (signal_calculator(k+1,List) - 1) or k == signal_calculator(k+1,List) or k == (signal_calculator(k+1,List) + 1):
+        Image.append('#')
+    else:
+        Image.append('.')
+
+for i,j in enumerate(Image):
+    print(j,end='')
+    if i % 40 == 39:
+        print('\n')
+
+for b in range(240):
+    print(signal_calculator(b+1,List),end=' ')
