54c4065aaf58dbcbe9847bea794b7f99e36bd339
Applee-Chen <Applee-Chen@users.noreply.github.com>
2022-12-19 21:41:19 +0000

    Day 7 task 1

---
+++ day7/day7.py
@@ -0,0 +1,42 @@
+'''Parse input and load the data into both tasks'''
+commands = []
+with open('day7.txt', 'r', encoding='utf-8') as file:
+    commands = file.readlines()
+
+def size_of_directories(command_file,name_of_directory):
+    '''Obtain size of a directory'''
+    count = 0
+    size_of_commands = int(len(command_file))
+    for rows in range(size_of_commands):
+        if command_file[rows].split(' ')[1] == 'cd':
+            a = '\n'
+            converted_name_of_directory = name_of_directory+a
+            if command_file[rows].split(' ')[2] == converted_name_of_directory:
+                '''Loop through the files or firectories in our desired directory'''
+                for subsidiaries in range(size_of_commands):
+                    if int(len(command_file[rows+subsidiaries+2].split(' '))) == 2:
+                        if command_file[rows+subsidiaries+2].split(' ')[0] != 'dir':
+                            count = count + int((command_file[rows+subsidiaries+2].split(' '))[0])
+                        else:
+                            moderated_name = (command_file[rows+subsidiaries+2].split(' ')[1])[:-1]
+                            count = count + size_of_directories(command_file,moderated_name)
+                    else:
+                        return count
+
+def task1(command_file):
+    '''identify all directories and write them into a list'''
+    sum_of_required_directories = 0
+    list_of_directories = []
+    size_of_file = len(command_file)
+    for rows in range(size_of_file):
+        if (command_file[rows].split(' '))[0] == 'dir':
+            directory_name = ((command_file[rows].split(' '))[1])[:-1]
+            if directory_name not in list_of_directories:
+                list_of_directories.append(directory_name)
+    number_of_directories = len(list_of_directories)
+    for directory in range(number_of_directories):
+        if size_of_directories(command_file,list_of_directories[directory]) <= 100000:
+            sum_of_required_directories = sum_of_required_directories + size_of_directories(command_file,list_of_directories[directory])
+    return sum_of_required_directories
+
+print(task1(commands))
