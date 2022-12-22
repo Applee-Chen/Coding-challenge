780bd50d335cc1ad1b30a8c42e889373b354cfb6
Applee-Chen <Applee-Chen@users.noreply.github.com>
2022-12-22 14:36:12 +0000

    day9 task1

--- day9/day9.py
+++ day9/day9.py
@@ -1,4 +1,9 @@
-
+def parse_grid(filename):
+    '''Read the data file into a list of movements'''
+    grid = []
+    with open(filename, 'r', encoding='utf-8') as file:
+        grid = file.readlines()
+    return grid
 
 def parse_input(filename):
     '''Read the data file into a list of movements'''
@@ -7,8 +12,13 @@ def parse_input(filename):
         movements = file.readlines()
     return movements
 
-def head_movement(movements_file, grid):
+def head_movements(instructions, grid):
     '''Instruct how the head will move according to the file'''
+    '''Instructions here should be only in one go: R,L,U,D'''
+    right = [1,0]
+    left = [-1,0]
+    up = [0,1]
+    down = [0,-1]
 
 
 
@@ -21,5 +31,6 @@ if __name__ == '__main__':
     # assert task1(day9_test_input) == 13140
     # assert task2(day9_test_input) == ''
     # print(task1(day9_input))
+    # grid = parse_grid('grid.txt')
 
 
